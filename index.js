e30c1b8c4660533198dcc4c51c08d705f425a3f6
Hornet07 <75621716+Hornet07@users.noreply.github.com>
2021-01-08 14:07:07 +0300

    eval

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const config = require('./config.json');
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+  if(!msg.content.startsWith(config.prefix)) return;
+  const args = msg.content.slice(config.prefix.length).trim().split(/ +/) //I have no idea what / +/ means lol
+  const cmd = args.shift()
+
+  if(command === 'eval') {
+    var result = eval(args[0])
+    msg.channel.send(result)
   }
+  //for(){}
 });
 
-client.login('Nzk2NjI5NTczNDI4MTgzMDYw.X_atGg.Ed5SnA20resjfL5WvJljTLXRnvw');
+client.login(process.env.TOKEN);
