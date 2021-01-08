21ae7c9748382b15fe77ebb0cc3e4473543e1829
Hornet07 <75621716+Hornet07@users.noreply.github.com>
2021-01-08 14:18:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,12 +6,19 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+var debug = true;
 client.on('message', msg => {
+  if(msg.content === 'debug') debug = !debug;
   if(!msg.content.startsWith(config.prefix)) return;
   const args = msg.content.slice(config.prefix.length).trim().split(/ +/) //I have no idea what / +/ means lol
   const cmd = args.shift()
 
-  if(command === 'eval') {
+  if(debug){
+    msg.channel.send(args)
+    msg.channel.send(cmd)
+  }
+  
+  if(cmd === 'eval') {
     var result = eval(args[0])
     msg.channel.send(result)
   }
