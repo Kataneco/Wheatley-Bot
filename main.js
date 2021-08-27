8077d9d6520951dfb1346c727905fe9341273a5d
Hornet07 <75621716+Hornet07@users.noreply.github.com>
2021-08-27 22:25:27 +0200

    :trollface:

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ const yts = require("yt-search");
 const fs = require("fs");
 const { stderr } = require("process");
 
-const client = new Discord.Client();
+const intents = new Discord.Intents([Discord.Intents.FLAGS.DIRECT_MESSAGES, Discord.Intents.FLAGS.DIRECT_MESSAGE_REACTIONS, Discord.Intents.FLAGS.DIRECT_MESSAGE_TYPING, Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, Discord.Intents.FLAGS.GUILD_MEMBERS, Discord.Intents.FLAGS.GUILD_MESSAGES, Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Discord.Intents.FLAGS.GUILD_MESSAGE_TYPING, Discord.Intents.FLAGS.GUILD_PRESENCES, Discord.Intents.FLAGS.GUILD_VOICE_STATES]);
+const client = new Discord.Client({intents: intents});
 var servers = {};
 
 client.on("message", message => {
